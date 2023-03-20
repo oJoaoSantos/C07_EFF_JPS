c74fcb05a3b8ebbe4dc8b7ebe8c557b5e65da820
oJoaoSantos <121128477+oJoaoSantos@users.noreply.github.com>
2023-03-20 22:32:09 +0000

    Add project files.

---
+++ D00_Utility/Utility.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Text;
+
+namespace D00_Utility
+{
+    public static class Utility
+    {
+        public static void SetUniCodeConsole() // Console Encoding (prepare console to recive special characters)
+        {
+            // Console.WriteLine("á Á à À ã Ã â Â ç Ç º ª");
+
+            Console.OutputEncoding = Encoding.UTF8;                 // Using System.Text 
+
+            // Console.WriteLine("á Á à À ã Ã â Â ç Ç º ª");
+        }
+
+        public static void WriteTitle(string title)
+        {
+            Console.WriteLine(new string('-', 50));
+            Console.WriteLine(title.ToUpper());
+            Console.WriteLine(new string('-', 50));
+        }
+
+        public static void WriteSubTitle(string subtitle)
+        {
+            Console.WriteLine(new string('-', 30));
+            Console.WriteLine(subtitle.ToUpper());
+            Console.WriteLine(new string('-', 30));
+        }
+
+        public static void TerminateConsole()
+        {
+            Console.Write("\n\nPrime qualquer tecla para saíres.");
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+        public static bool ValidateNumber(double value)
+        {
+            // v1 não otimizado
+
+            //bool recived = false;
+            //if (value == 0)
+            //{
+            //    recived = true; 
+            //}
+            //return recived;
+
+            // v2 Operador trenário
+
+            // return value == 0 ? true : false;
+
+            // v3 Mais otimizado
+
+            return value != 0;
+        }
+
+        public static bool ValidateStringDouble(string value)
+        {
+            return double.TryParse(value, out double valueConverted);
+        }
+
+        public static bool ValidateStringInt(string value)
+        {
+            return int.TryParse(value, out int valueConverted);
+        }
+
+        public static bool ValidateNumberDouble(string value)
+        {
+
+            double valueConverted;
+
+            return double.TryParse(value, out valueConverted) && valueConverted != 0;
+
+        }
+
+        internal static void TestInternal()
+        {
+            Console.WriteLine("TestInternal");
+        }
+
+        public static void BlockSeparator(int enters)
+        {
+            Console.WriteLine(new string('\n', enters));
+        }
+    }
+}
---
+++ D01_EF6_DF/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using D00_Utility;
+
+namespace D01_EF6_DF
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Utility.SetUniCodeConsole();
+
+            #region 1. Declarar o Contexto de BD (Instanciar a DB Context)
+            var db = new NorthwindEntities();                           // permite-me aceder à conection string e aos DBSets
+            #endregion
+
+            #region 2. Usar o contexto de BD
+            using (db)
+            {
+                
+                #region Nova Region (1)
+                Region region01 = new Region();
+                Region region02 = new Region();
+                Region region03 = new Region();
+
+                region01.RegionID = 5;                                   // não é identity
+                region01.RegionDescription = "Norte";
+
+                region02.RegionID = 6;                                   
+                region02.RegionDescription = "Centro";
+
+                region03.RegionID = 7;                                   
+                region03.RegionDescription = "Sul";
+
+                db.Region.Add(region01);                                 // adicionar o novo registo à tabela em memória (dbset)
+                db.Region.Add(region02);                                 
+                db.Region.Add(region03);                                 
+
+                int contRegions = db.SaveChanges();                       // gravar o novo resgisto na bd.
+
+                Utility.WriteTitle("Region");
+                Console.WriteLine($"{contRegions} nova(s) região/regiões gravada(s). \n");
+
+                //Linq para listar as regiões (id - description)
+                var queryRegion = db.Region.Select(r => r).OrderBy(r => r.RegionID);
+                
+                foreach (var item in queryRegion)
+                {
+                    Console.WriteLine($"{item.RegionID} - {item.RegionDescription}");
+                }
+                #endregion
+                
+                #region Novo Territory da Region (n)
+                Territories territorie01 = new Territories();
+                Territories territorie02 = new Territories();
+                Territories territorie03 = new Territories();
+
+                territorie01.TerritoryID = "00001";
+                territorie01.TerritoryDescription = "Porto";
+                territorie01.RegionID = 5;
+
+                territorie02.TerritoryID = "00002";
+                territorie02.TerritoryDescription = "Coimbra";
+                territorie02.RegionID = 6;
+
+                territorie03.TerritoryID = "00003";
+                territorie03.TerritoryDescription = "Portimão";
+                territorie03.RegionID = 7;
+
+                db.Territories.Add(territorie01);
+                db.Territories.Add(territorie02);
+                db.Territories.Add(territorie03);
+
+                int contTerritories = db.SaveChanges();
+
+                Utility.BlockSeparator(2);
+                Utility.WriteTitle("Territory");
+                Console.WriteLine($"{contTerritories} novo(s) território(s) gravado(s). \n");
+
+                var queryTerritory = db.Territories.Select(t => t).OrderByDescending(t => t.TerritoryID);
+
+                queryTerritory.ToList().ForEach(t => Console.WriteLine($"{t.TerritoryID} - {t.RegionID} - {t.TerritoryDescription}"));
+                #endregion
+            }
+            #endregion
+
+            Utility.TerminateConsole();
+        }
+    }
+}
